18e2e3f365b4af8d56abe5c7c70337b53f02685d
Joshua Strobl <joshua@streambits.io>
2020-01-23 16:55:20 +0200

    Tag 27.0 for Plasma Wallpaper

--- mkrelease.sh
+++ mkrelease.sh
@@ -6,7 +6,7 @@ meson --prefix /usr build
 ninja dist -C build
 
 # Bump in tandem with meson.build, run script once new tag is up.
-VERSION="26"
+VERSION="27"
 TAR="artwork-${VERSION}.tar.xz"
 mv build/meson-dist/$TAR .
 
